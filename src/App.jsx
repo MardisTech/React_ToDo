b0ea936558ed7808e09a47c99a95fa14272019f6
Alex Mardis <mardistech@gmail.com>
2023-08-10 16:01:44 -0400

    working, storing notes in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NewTodoForm } from "./NewTodoForm"
 import "./styles.css"
+import { TodoList } from "./TodoList"
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
-    setTodos((currentTodos) => {
+    setTodos(currentTodos => {
       return [
-        ...currentTodos, 
+        ...currentTodos,
         { id: crypto.randomUUID(), title, completed: false },
       ]
     })
@@ -18,44 +28,25 @@ export default function App() {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
         if (todo.id === id) {
-          return { ...todo, completed}
+          return { ...todo, completed }
         }
 
-        return todo 
-
+        return todo
       })
     })
   }
 
   function deleteTodo(id) {
     setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id )
+      return currentTodos.filter(todo => todo.id !== id)
     })
   }
 
-  console.log(todos)
-
   return (
     <>
-      <NewTodoForm onSubmit={addTodo}/>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {todos.length === 0 && "No Todos"}
-        {todos.map(todo => {
-          return (
-            <>
-            <li key={todo.id}>
-              <label>
-                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
-                {todo.title}
-              </label>
-              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-            </li>
-            </>
-          )
-        })}
-        
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
